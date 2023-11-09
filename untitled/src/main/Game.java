5a70409a05dc8e639c1a9132671d3376b291b1ac
MrHerbat <113918343+MrHerbat@users.noreply.github.com>
2023-11-09 12:57:18 +0100

    Add files via upload

---
+++ untitled/src/main/Game.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Game
+{
+
+    private Window window;
+    private  Panel panel;
+
+
+    public Game()
+    {
+        panel = new Panel();
+        window = new Window(panel);
+        panel.requestFocus();
+    }
+}
