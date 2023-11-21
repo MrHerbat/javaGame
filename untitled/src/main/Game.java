1b807d72bb297dde29c675a48dc6c0d50b0c6142
MrHerbat <113918343+MrHerbat@users.noreply.github.com>
2023-11-21 12:25:16 +0100

    Update Game.java

--- untitled/src/main/Game.java
+++ untitled/src/main/Game.java
@@ -1,16 +1,51 @@
 package main;
 
-public class Game
+public class Game implements Runnable
 {
 
     private Window window;
     private  Panel panel;
+    private Thread gameThread;
+    private final int FPS_SET=120;
 
 
     public Game()
     {
         panel = new Panel();
+        startGameLoop();
         window = new Window(panel);
         panel.requestFocus();
     }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now;
+
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        while (true) {
+
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                panel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+    }
+
+
 }
