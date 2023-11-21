93d28190b37b02496c72f0dfdea0218f12d8439a
MrHerbat <113918343+MrHerbat@users.noreply.github.com>
2023-11-21 09:56:48 +0100

    Update Panel.java

--- untitled/src/main/Panel.java
+++ untitled/src/main/Panel.java
@@ -7,24 +7,28 @@ import java.awt.event.KeyListener;
 
 public class Panel extends JPanel implements KeyListener
 {
-    public int choice = 0;
-    public String[] tekst = {"Attack","Defend", "Items", "Run",""};
+    public int choice = 0, cChoice=0;
+    public String[] tekst = {"Attack","Defend", "Items", "Run"};
     private int frames;
     private long lastCheck = 0;
     public JLabel label1 = new JLabel();
+    private Color[] color = {new Color(0,0,0),new Color(127,50,50)};
 
     public Panel()
     {
         this.addKeyListener(this);
         this.setFocusable(true);
         this.requestFocusInWindow();
+        this.add(label1);
     }
     public void paintComponent(Graphics graphics)
     {
         super.paintComponent(graphics);
-
+        graphics.setColor(Color.BLACK);
         graphics.fillRect(100,200,50,50);
+        graphics.setColor(color[cChoice]);
         graphics.fillRect(300,200,50,50);
+        label1.setLocation(215, 350);
         label1.setText(tekst[choice]);
 
 
@@ -66,6 +70,14 @@ public class Panel extends JPanel implements KeyListener
                     choice=3;
                 }
                 break;
+                case KeyEvent.VK_ENTER:
+                    if(choice == 0)
+                    {
+                        cChoice = 1;
+                    }else{
+                        cChoice = 0;
+                    }
+                    break;
         }
 
     }
